8e84e65292e0bb2b7f4706fa42b8a9b003030531
bhuang <Terry1987101@163.com>
2016-05-30 10:23:01 +0800

    【环境配置】新建数据库连接组件

---
+++ common/config/db.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Chuang
+ * Date: 2015/5/17
+ * Time: 20:57
+ */
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=121.43.96.68;dbname=acc',
+    'username' => 'acc',
+    'password' => 'acc@admin@626',
+    'tablePrefix' => 'tb_',
+    'charset' => 'utf8'
+];
