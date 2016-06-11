5f86c6b97e23f4cba311ebacc93e1c89a29756e1
terry <terry1987101@163.com>
2016-06-11 10:45:39 +0800

    【Dev】增加本地数据库设置

--- common/config/db.php
+++ common/config/db.php
@@ -7,9 +7,12 @@
  */
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=121.43.96.68;dbname=acc',
-    'username' => 'acc',
-    'password' => 'acc@admin@626',
+//    'dsn' => 'mysql:host=121.43.96.68;dbname=acc',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=acc',
+//    'username' => 'acc',
+    'username' => 'root',
+//    'password' => 'acc@admin@626',
+    'password' => '',
     'tablePrefix' => 'tb_',
     'charset' => 'utf8'
 ];
